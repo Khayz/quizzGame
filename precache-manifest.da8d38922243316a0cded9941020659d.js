a73509bb474b2513b60daa7c22d3b0ac8fd12715
Khay <altaircarn@gmail.com>
2020-03-04 14:03:11 -0600

    Updates

---
+++ precache-manifest.da8d38922243316a0cded9941020659d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e64916c1b83716290e4106956f2ca63",
+    "url": "/quizzGame/index.html"
+  },
+  {
+    "revision": "18deb2b6734ade1a1d8c",
+    "url": "/quizzGame/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "142230fd5669fc419f46",
+    "url": "/quizzGame/static/js/2.b6bf0ae8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/quizzGame/static/js/2.b6bf0ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18deb2b6734ade1a1d8c",
+    "url": "/quizzGame/static/js/main.d2aa9243.chunk.js"
+  },
+  {
+    "revision": "eaad377d2e9d34d33d8d",
+    "url": "/quizzGame/static/js/runtime-main.f5a735f9.js"
+  }
+]);
